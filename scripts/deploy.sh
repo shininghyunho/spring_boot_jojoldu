37288760ebfbb523e7ecffbf24e7930b602facc2
hyunho <gusgh3315@gmail.com>
2021-04-27 01:50:11 +0900

    add /

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=spring-boot-jojoldu-webservice
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl spring-boot-jojoldu-webservice | grep jar | awk '{print $1}')
