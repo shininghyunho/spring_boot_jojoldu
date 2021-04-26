19aa9dd1082edbda1f5c9c583a7918f608266f20
hyunho <gusgh3315@gmail.com>
2021-04-27 02:14:02 +0900

    add current pid name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl spring-boot-jojoldu-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl spring_boot_jojoldu | grep java | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
